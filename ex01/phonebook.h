18d62196f632df531b6718bd17f991015fcd29dc
erijania <erijania@student.42antananarivo.mg>
2024-12-29 10:39:37 +0300

    ex01: ajout d'un header générique pour tout le programme

---
+++ ex01/phonebook.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erijania <erijania@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/29 10:25:18 by erijania          #+#    #+#             */
+/*   Updated: 2024/12/29 10:28:00 by erijania         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+# define MAX_CONTACT_RECORD 8
+# define TRUNC_AT 10
+
+#endif
